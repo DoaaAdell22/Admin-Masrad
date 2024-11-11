25240babcacdf5a9a52a2417e7caa941100a20fe
unknown <doaa>
2024-11-12 01:35:03 +0200

    themes

--- src/store/themeSwitcher/reducer.ts
+++ src/store/themeSwitcher/reducer.ts
@@ -1,20 +1,18 @@
-
 import actions from './actions';
-import {  isCurrentThemeDark } from './config';
-const defaultTheme = 'system'
+import { isCurrentThemeDark } from './config';
+
+const defaultTheme = 'dark'; 
 const initState = {
   themeName: defaultTheme,
-  isDark:isCurrentThemeDark(defaultTheme)
+  isDark: isCurrentThemeDark(defaultTheme) 
 };
 
 export default function ThemesSwitcher(state = initState, action) {
   switch (action.type) {
     case actions.CHANGE_THEME:
-      // localStorage.setItem('theme' ,action.theme)
       return {
         themeName: action.payload.theme,
-        isDark:isCurrentThemeDark(action.payload.theme)
-
+        isDark: isCurrentThemeDark(action.payload.theme)
       };
     default:
       return state;
